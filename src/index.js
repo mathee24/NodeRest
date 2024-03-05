d63908bfa328b1c14523b4b1d8b4f486b93cabe3
mathee24 <s6506021620156@email.kmutnb.ac.th>
2024-03-05 09:45:25 +0700

    ss

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const e = require('express');
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const app = express();
+const cors = require('cors');
 
 const db = new sqlite3.Database('./Database/Book.sqlite');
 
 app.use (express.json());
+app.use(cors());
 
 db.run(`CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY,
